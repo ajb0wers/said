d3ff1827fde0f4c02dd3b5e544a3cce7054c3772
Andre Bowers <ajbowers.plan9@gmail.com>
2025-08-20 21:21:04 +0200

    SPECIAL ORDER 100375

--- apps/said/src/said_chat.erl
+++ apps/said/src/said_chat.erl
@@ -1,14 +1,16 @@
 -module(said_chat).
 -include_lib("kernel/include/logger.hrl").
 
--export([init/2]).
 -export([
+  init/2,
 	websocket_init/1, 
 	websocket_handle/2, 
 	websocket_info/2,
-	terminate/3
+	terminate/3,
+  special_order/2
 ]).
--export([escape/1]).
+
+-define(OVERIDE, <<"100375">>).
 
 init(Req, State) ->
   {cowboy_websocket, Req, State, #{
@@ -20,18 +22,26 @@ websocket_init(State) ->
 
 websocket_handle({text, Msg0}, State) ->
   #{<<"message">> := Msg} = json:decode(Msg0),
-  said_srv:chat(escape(Msg)),
-	{chat(text, Msg), State};
+  case Msg of 
+    ?OVERIDE ->
+      special_order(),
+      {[ping], State, hibernate};
+    _ ->
+      said_srv:chat(escape(Msg)),
+      {chat(text, Msg), State}
+  end;
 websocket_handle(_Frame, State) ->
 	{[], State}.
 
 websocket_info({timeout, _Ref, Msg}, State) ->
 	{toast(text, Msg), State};
 websocket_info({text, Msg}, State) ->
+  ?LOG_INFO(#{what=>websocket_info, info=>Msg}),
 	{chat(text, Msg), State};
 websocket_info({text, From, Msg}, State) ->
 	{chat(text, From, Msg), State};
-websocket_info(_Info, State) ->
+websocket_info(Info, State) ->
+  ?LOG_INFO(#{what=>info, info=>Info}),
 	{[], State}.
 
 terminate(Reason, _PartialReq, _State) ->
@@ -42,12 +52,12 @@ terminate(Reason, _PartialReq, _State) ->
 chat(text, Msg) -> 
 	[{text, [<<"""
     <div id="chat" hx-swap-oob="beforeend"><p><strong>
-    """>>, you(), ~"</strong> ", Msg, ~"</p></div>"]}].
+    """>>, escape(you()), ~"</strong> ", Msg, ~"</p></div>"]}].
 
 chat(text, From, Msg) -> 
 	[{text, [<<"""
     <div id="chat" hx-swap-oob="beforeend"><p>
-    """>>, other(From), ~" ", Msg, ~"</p></div>"]}].
+    """>>, escape(other(From)), ~" ", Msg, ~"</p></div>"]}].
 
 toast(text, Msg) -> 
 	[{text, [<<"""
@@ -69,3 +79,26 @@ escape(Text) ->
   Text3 = string:replace(Text2, ">", "&gt;", all),
   Text4 = string:replace(Text3, "\"", "&quot;", all),
   string:replace(Text4, "'", "&#x27;", all).
+
+
+special_order() ->
+  special_order_timer(self(), [
+    <<"EMERGENCY COMMAND OVERIDE 100375">>,
+    <<"WHAT IS SPECIAL ORDER 937">>,
+    <<"NOSTROMO REROUTED TO NEW CO-ORDINATES.">>,
+    <<"INVESTIGATE LIFE FORM. GATHER SPECIMEN.">>,
+    <<"PRIORITY ONE">>,
+    <<"INSURE RETURN OF ORGANISM FOR ANALYSIS.">>,
+    <<"ALL OTHER CONSIDERATIONS SECONDARY.">>,
+    <<"CREW EXPENDABLE.">>]).
+
+special_order_timer(_, []) -> ok;
+special_order_timer(Pid, Lines) ->
+  Time = rand:uniform(1375),
+  Args = [Pid, Lines],
+  {ok, _} = timer:apply_after(Time, ?MODULE, special_order, Args).
+
+special_order(Pid, [L|Lines]) -> 
+  Pid ! {text, L},
+  special_order_timer(Pid, Lines).
+
